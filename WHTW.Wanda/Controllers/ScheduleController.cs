0400f9f5199db849d60a4487827d7949f23e9782
betobarros07 <betobarros07@gmail.com>
2017-03-12 00:50:20 -0300

    Schedule added.

--- WHTW.Wanda/Controllers/ScheduleController.cs
+++ WHTW.Wanda/Controllers/ScheduleController.cs
@@ -7,18 +7,46 @@ using System.Web.Http;
 
 namespace WHTW.Wanda.Controllers
 {
+    using Filters;
     using Models;
+    using ViewModels.Schedule;
 
+    [Authorize]
     [RoutePrefix("api")]
     public class ScheduleController : ApiController
     {
         [HttpGet]
         [Route("hospital/{hospitalId}/schedule")]
-        public IHttpActionResult List(Guid hospitalId)
+        public IHttpActionResult List(Guid hospitalId, string date = null)
         {
             using (var dbContext = new AppContext())
             {
-                return Ok();
+                DateTime dateFilter;
+                if (string.IsNullOrEmpty(date))
+                    dateFilter = DateTime.Now.Date;
+                else
+                    dateFilter = Convert.ToDateTime(date);
+                return Ok(dbContext.Schedule.Where(a => a.Date.Date == dateFilter).ToList());
+            }
+        }
+
+        [HttpPost]
+        [Route("hospital/{hospitalId}/schedule")]
+        [ModelStateValid]
+        public IHttpActionResult Create(Guid hospitalId, CreateSchedule model)
+        {
+            using (var dbContext = new AppContext())
+            {
+                var schedule = new Schedule
+                {
+                    CreatedAt = DateTime.Now,
+                    Date = model.Date,
+                    HospitalId = hospitalId,
+                    Id = Guid.NewGuid(),
+                    UserId = Util.GetUserId()
+                };
+                dbContext.Schedule.Add(schedule);
+                return Created("/me/schedules", schedule);
             }
         }
     }
---
+++ WHTW.Wanda/ViewModels/Schedule/CreateSchedule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WHTW.Wanda.ViewModels.Schedule
+{
+    public class CreateSchedule
+    {
+        [Required(ErrorMessage = "Date is required.")]
+        public DateTime Date { get; set; }
+    }
+}
